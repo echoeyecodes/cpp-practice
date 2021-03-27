abf28966da8302ac7276e3113c4f7d6a6e8738d8
Oluwafemi Obajuluwa <femiobajuluwa@gmail.com>
2021-03-27 11:18:25 +0100

    cpp has lamdas! removed creating new copies of vectors in memory

--- map-filter-reduce.cpp
+++ map-filter-reduce.cpp
@@ -6,63 +6,58 @@ using namespace std;
 
 
 template<typename T>
-void forEach(vector<T>* values, void(*function)(T)) {
-		vector<T> list = *values;
-		for (auto item : list) {
-			function(item);
-		}
+void forEach(vector<T>& values, void(*function)(T)) {
+	for (auto item : values) {
+		function(item);
+	}
 };
 
 
 template<typename T>
-vector<T> map(vector<T>* values, T(*function)(T)) {
-		vector<T> list = *values;
-		vector<T> new_array = {};
-		for (auto item : list) {
-			auto newItem = function(item);
-			new_array.push_back(newItem);
-		}
-		return new_array;
+vector<T> map(vector<T>& values, T(*function)(T)) {
+	vector<T> new_array = {};
+	for (auto item : values) {
+		auto newItem = function(item);
+		new_array.push_back(newItem);
+	}
+	return new_array;
 };
 
 
 template<typename T>
-vector<T> filter(vector<T>* values, bool(*function)(T)) {
-		vector<T> list = *values;
-		vector<T> new_array = {};
-		for (auto item : list) {
-			bool match = function(item);
-			if (match) {
-				new_array.push_back(item);
-			}
+vector<T> filter(vector<T>& values, bool(*function)(T)) {
+	vector<T> new_array = {};
+	for (auto item : values) {
+		bool match = function(item);
+		if (match) {
+			new_array.push_back(item);
 		}
-		return new_array;
+	}
+	return new_array;
 };
 
 
 template<typename T>
-vector<T> flatMap(vector<vector<T>>* values, vector<T>(*function)(vector<T>)) {
-		vector<vector<T>> list = *values;
-		vector<T> new_array = {};
-		for (auto item : list) {
-			auto newItem = function(item);
-			for (auto item1 : newItem) {
-				new_array.push_back(item1);
-			}
+vector<T> flatMap(vector<vector<T>>& values, vector<T>(*function)(vector<T>)) {
+	vector<T> new_array = {};
+	for (auto item : values) {
+		auto newItem = function(item);
+		for (auto item1 : newItem) {
+			new_array.push_back(item1);
 		}
-		return new_array;
+	}
+	return new_array;
 };
 
 template<typename T, typename U>
-U reduce(vector<T>* values, U(*function)(U, T, int), U init) {
-		vector<T> list = *values;
-		auto new_value = init;
-		int count = 0;
-		for (auto item : list) {
-			new_value = function(new_value,item, count);
-			count++;
-		}
-		return new_value;
+U reduce(vector<T>& values, U(*function)(U, T, int), U init) {
+	auto new_value = init;
+	int count = 0;
+	for (auto item : values) {
+		new_value = function(new_value, item, count);
+		count++;
+	}
+	return new_value;
 };
 
 
@@ -94,11 +89,11 @@ int main() {
 	vector<vector<int>> scores1 = { {0,1}, {1,2,3,5,4}, {5} };
 	vector<string> scores2 = { "A", "E", "I", "O", "U" };
 	string s = "";
-	auto d = flatMap<int>(&scores1, doFlatMap);
 
-	for (auto item : d) {
-		cout << item << endl;
-	}
+	auto d = reduce<string, int>(scores2, [](int acc, string next, int index) {
+		return int(acc + next.size());
+		}, 0);
+		cout << d << endl;
 	return 0;
 
 }
